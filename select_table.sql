b5ebb98527941c1b8a7011a516217e3e2c44c92f
Ikhwalfebriyo <ikhwalfebpra@gmail.com>
2023-11-12 12:37:18 +0700

    select table

---
+++ select_table.sql
@@ -0,0 +1,7 @@
+select * from pelanggan;
+select * from kategori;
+select * from barang;
+select * from transaksi;
+select * from barang where harga_barang > 100000;
+select id, nama, alamat, no_hp from pelanggan;
+select * from transaksi where tanggal_transaksi = "2023-11-10";
