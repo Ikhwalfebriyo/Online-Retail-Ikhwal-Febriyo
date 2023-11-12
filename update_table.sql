977ffb3e87a919ac28f7d918d1a02c19c11811b3
Ikhwalfebriyo <ikhwalfebpra@gmail.com>
2023-11-12 12:37:48 +0700

    update table

---
+++ update_table.sql
@@ -0,0 +1,4 @@
+update pelanggan set alamat = "Pariaman" where id = 7;
+update kategori set nama_kategori = "Alat Olahraga" where id = 4;
+update barang set stok_barang = 50 where id = 1;
+update transaksi set tanggal_transaksi = "2023-11-06" where id = 11;
